6a3e9004c0d03b57e1c525e4d92e4f803cc6085e
Janvi-Ashara <janviashara16@gmail.com>
2025-03-07 18:21:59 +0530

    Javascript

--- loops/Map.js
+++ loops/Map.js
@@ -1,59 +1,50 @@
-const coding = ["js", "java", "python", "css", "asp"];
-
-const values = coding.forEach((item) => {
-// console.log(item);
-});
-
-console.log(values);
-
-// const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-// const newNums = myNums.filter((num) => num > 4);
-// console.log(newNums)
-
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const newNums = myNums.filter((num) => {
-    return num > 4      //without return keyword output is []
-});                //with return [ 5, 6, 7, 8, 9, 10 ]
-console.log(newNums)
 
-// or 
+// const newNum = myNums.map((num) => {
+//     return  =num + 10}
+// );
 
-// const newNums = []
+const newNum = myNums
+  .map((num) => num * 10)
+  .map((num) => num + 1)
+  .filter((num) => num >= 40);
 
-// myNums.forEach((num) => {
-//     if(num>4){
-//         newNums.push(num)
-//     }
+console.log(newNum);
+
+// myNums.forEach((item) =>{
+//     console.log(item)
+//     return item
 // })
-// console.log(newNums) //[ 5, 6, 7, 8, 9, 10 ]
 
-const books = [
-    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
-    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
-    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
-    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
-    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
-    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
-    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
-    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
-    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+// The reduce() method in JavaScript is used to reduce an array to a single value like:
+
+// ✅ Sum
+// ✅ Product
+// ✅ Concatenation
+// ✅ Average
+
+const nums = [1, 2, 3];
+const myTotal = nums.reduce(function (acc, current) {
+  return acc + current;
+}, 0); // 0 is initization
+console.log(myTotal); // 6
+
+const Total = myNums.reduce((acc, current) => acc + current, 0);
+console.log(myTotal);
+
+const shoppingCart = [
+  {
+    itemName: "jS cource",
+    price: 2999,
+  },
+  {
+    itemName: "Data science",
+    price: 3999,
+  },
+  {
+    itemName : "python",
+    price: 9000,
+  }
 ];
-
-books.forEach((item) => {
-    console.log(item.title,item.genre,item.edition)
-})
-
-let UserBooks = books.filter((bk)=>{
-    return bk.genre === 'History'
-})
-console.log(UserBooks)
-
-UserBooks = books.filter((bk)=>{
-    return bk.publish >= 1995 && bk.genre === 'History'})
-console.log(UserBooks)
-
-const nonFiction = books.filter((book) => book.genre === 'Non-Fiction');
-console.log(nonFiction);
-
-
-
+const price = shoppingCart.reduce((acc, item) => acc + item.price,0)
+console.log(price)
